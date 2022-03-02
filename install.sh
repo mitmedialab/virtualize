e82d445b6eb38458e10188136eeb6897db2eb8d0
Jon Ferguson <jon@jef.us>
2022-03-02 17:26:51 -0500

    install.sh now inits submodules after a cloning, made activate more robust to not being sourced from a specific spot

--- install.sh
+++ install.sh
@@ -1,46 +1,55 @@
 #!/bin/bash
 
-if [[ -e activate ]]; then
-    echo "./activate script already exists, not installing"
+if [[ ! $BASH_SOURCE ]]; then
+    echo "You cannot source this script. Run it as ./$0" >&2
     exit 33
 fi
 
-ln -s virtualize/activate ./activate
-echo "installed ./activate file"
+# going to compute our own local (non-exported) VIRTUALIZE_ROOT and ignore any preset one
+VIRTUALIZE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE}" )" &> /dev/null && pwd )
+VIRTUALIZE_ROOT=$( dirname -- "${VIRTUALIZE_DIR}" )
 
+for install in $VIRTUALIZE_ROOT/virtualize*/install.sh; do
+    if [[ $install == "$VIRTUALIZE_ROOT/virtualize/install.sh" ]]; then
+       continue
+    fi
+    shortname=$( dirname $install | sed 's/^virtualize-//' )
+    echo "installing $shortname"
+    ( $install ) >/dev/null
+done
 
-
-if [[ -e install.sh ]]; then
-    echo "install.sh exists, skipping making install.sh template"
-    exit
+if [[ -e $VIRTUALIZE_ROOT/activate ]]; then
+    echo "./activate script already exists, not installing it"
+else
+    ln -s virtualize/activate $VIRTUALIZE_ROOT/activate
+    echo "installed ./activate file"
 fi
 
-cat >>install.sh <<-EOF
-#!/bin/sh
+if [[ -e $VIRTUALIZE_ROOT/install.sh ]]; then
+    echo "install.sh exists, skipping making install.sh template"
+else
+    cat >>$VIRTUALIZE_ROOT/install.sh <<-EOF
+	#!/bin/sh
 
-# install git submodules
-git submodule init
-git submodule update
+	# install git submodules
+	git submodule init
+	git submodule update
+	virtualize/install.sh
 
-### as you add virtualize modules (node, python, etc...) you will want to
-### edit the following sections and enable them
+	### as you add virtualize modules (node, python, etc...) you will
+	### probably want to edit the following sections and enable them
 
-###
-### node
-###
+	### node
+	# yarn install
 
-# yarn install
-
-###
-### python
-###
-
-# pip install -f requirments.txt
+	### python
+	# pip install -f requirments.txt
 EOF
+    chmod +x $VIRTUALIZE_ROOT/install.sh
 
-chmod +x install.sh
+    echo "added install.sh template, edit it to suit your project"
+fi
 
-echo "added install.sh template, edit it to suit your project"
 echo
 echo "done installing virtualize"
 echo "be sure to commit the new files including .gitmodules"
