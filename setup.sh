0d06851155bdde37ee30ff61c134d92e8221f2a1
Jon Ferguson <jon@jef.us>
2023-07-20 14:05:03 -0400

    handle directories with spaces in the name, use tabs in heredoc so the indentation is stripped

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ fi
 VIRTUALIZE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE}" )" &> /dev/null && pwd )
 VIRTUALIZE_ROOT=$( dirname -- "${VIRTUALIZE_DIR}" )
 
-for setup in $VIRTUALIZE_ROOT/virtualize*/setup.sh; do
+for setup in "$VIRTUALIZE_ROOT"/virtualize*/setup.sh; do
     if [[ $setup == "$VIRTUALIZE_ROOT/virtualize/setup.sh" ]]; then
        continue
     fi
@@ -18,56 +18,58 @@ for setup in $VIRTUALIZE_ROOT/virtualize*/setup.sh; do
     ( $setup ) >/dev/null
 done
 
-if [[ -e $VIRTUALIZE_ROOT/activate ]]; then
+if [[ -e "$VIRTUALIZE_ROOT"/activate ]]; then
     echo "./activate script already exists, not installing it"
 else
-    ln -s virtualize/activate $VIRTUALIZE_ROOT/activate
+    ln -s virtualize/activate "$VIRTUALIZE_ROOT"/activate
     echo "installed ./activate file"
 fi
 
-if [[ -e $VIRTUALIZE_ROOT/setup.sh ]]; then
+if [[ -e "$VIRTUALIZE_ROOT"/setup.sh ]]; then
     echo "setup.sh exists, skipping making setup.sh template"
 else
-    cat >>$VIRTUALIZE_ROOT/setup.sh <<-EOF
-        #!/bin/sh
+    # note: tabs are part of the indention removal for heredocs, don't untabify
+    # https://stackoverflow.com/a/33817423
+    cat >>"$VIRTUALIZE_ROOT"/setup.sh <<-EOF
+	#!/bin/sh
 
-        #export VIRTUALIZE_NODE_VERSION=16.13.2
-        #export VIRTUALIZE_PYTHON_VERSION=3
+	#export VIRTUALIZE_NODE_VERSION=16.13.2
+	#export VIRTUALIZE_PYTHON_VERSION=3
 
-        # install git submodules
-        git submodule init
-        git submodule update
-        virtualize/setup.sh
+	# install git submodules
+	git submodule init
+	git submodule update
+	virtualize/setup.sh
 
-        ### as you add virtualize modules (node, python, etc...) you will
-        ### probably want to edit the following sections and enable them
+	### as you add virtualize modules (node, python, etc...) you will
+	### probably want to edit the following sections and enable them
 
-        source ./activate
+	source ./activate
 
-        ### node
-        if [[ -f package.json && -d virtualize-node ]]; then
-            yarn install
-        fi   
+	### node
+	if [[ -f package.json && -d virtualize-node ]]; then
+	    yarn install
+	fi   
 
-        ### python
-        if [[ -f requirements.txt && -d virtualize-python ]]; then
-            pip install -f requirments.txt
-        fi
+	### python
+	if [[ -f requirements.txt && -d virtualize-python ]]; then
+	    pip install -f requirments.txt
+	fi
 
-        ### miniconda
-        if [[ -f environment.yml && -d virtualize-miniconda ]]; then
-            conda env create --file $VIRTUALIZE_ROOT/environment.yml
-        fi
+	### miniconda
+	if [[ -f environment.yml && -d virtualize-miniconda ]]; then
+	    conda env create --file "$VIRTUALIZE_ROOT"/environment.yml
+	fi
 
-        ### homebrew
-        if [[ -f Brewfile && -d virtualize-homebrew ]]; then
-            brew bundle --file Brewfile
-        fi
+	### homebrew
+	if [[ -f Brewfile && -d virtualize-homebrew ]]; then
+	    brew bundle --file Brewfile
+	fi
 
-        ### macports
-        # unknown
+	### macports
+	# unknown
 EOF
-    chmod +x $VIRTUALIZE_ROOT/setup.sh
+    chmod +x "$VIRTUALIZE_ROOT"/setup.sh
 
     echo "added initial setup.sh script, edit it to suit your project"
 fi
